da4f809fd09a5f9260eff219977d42be37b4413d
IT22102546 <IT22102546@users.noreply.github.com>
2026-06-10 09:15:28 +0530

    New changes

---
+++ Client/Apri/Connected Accounts CSA/AquaForno.js
@@ -0,0 +1,9 @@
+<script src="https://devstorage.emojot.com/files/Widget/EmojotWebWidget.min.js"
+    data-company-id="699e8972fd4d75e16fc96e59" data-web-id="o9CXtudpj0hKe8oX63" data-auth-token="abcd"
+    data-primary-color="#9a2362" data-secondary-color="#d09abf" data-header="Welcome to our support chat!"
+    data-form-option="false"
+    data-avatar-image="https://d11m1m35uzfps.cloudfront.net/pictures/skinImages/59747e53cd90e9231b90de48-skin211.png"
+    data-welcome-image="https://storage.emojot.com/logos/emojot/favicon.png"
+    data-user-avatar="https://d11m1m35uzfps.cloudfront.net/pictures/generalImages/59747e53cd90e9231b90de48-skin130.png"
+    data-lottie-url="https://lottie.host/c71a70f9-627a-4ff0-9d5f-71c5c65a53a6/S8L8oRQOrP.lottie">
+</script>
